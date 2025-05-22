a3b2bff08408837a302b974d79426a2e919190ac
shangchenguang <shangchenguang@didiglobal.com>
2025-05-22 18:11:21 +0800

    init

---
+++ free-chart/src/main/java/org/scg/freechart/FreeChartApplication.java
@@ -0,0 +1,13 @@
+package org.scg.freechart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeChartApplication.class, args);
+	}
+
+}
---
+++ free-chart/src/test/java/org/scg/freechart/FreeChartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scg.freechart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeChartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
